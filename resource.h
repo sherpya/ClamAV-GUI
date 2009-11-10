ac9eca6e3405ff9a49e52f36cb05bc009fb3b910
Gianluigi Tiesi <sherpya@users.sourceforge.net>
2009-11-10 00:44:29 +0000

    enabled minimize box

--- resource.h
+++ resource.h
@@ -1,40 +1,23 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by ClamAV-GUI.rc
-//
-
-#define IDI_ICON                        100
-#define IDB_BITMAP                      101
-
-#define IDD_CLAMAVGUI_DIALOG            102
-#define IDD_MAIN                        103
-
-#define IDC_BROWSE                      1000
-#define IDC_UPDATE                      1001
-#define IDC_SCAN                        1002
-#define IDC_CANCEL                      1003
-#define IDC_TARGET                      1004
-#define IDC_ALLDRIVES                   1005
-#define IDC_RECURSE                     1006
-#define IDC_NOARCHIVES                  1007
-#define IDC_ONLYINFECTED                1008
-#define IDC_REMOVE                      1009
-#define IDC_NOMAIL                      1010
-#define IDC_NOPE                        1011
-#define IDC_NOOLE2                      1012
-#define IDC_NOHTML                      1013
-#define IDC_STATUS                      1014
-#define IDC_CMDLINE                     1015
-#define IDC_STATIC                      -1
-
-// Next default values for new objects
-//
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1016
-#define _APS_NEXT_SYMED_VALUE           110
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
 #endif
-#endif
+
+#define IDI_ICON                                100
+#define IDB_BITMAP                              101
+#define IDD_MAIN                                103
+#define IDC_BROWSE                              1000
+#define IDC_UPDATE                              1001
+#define IDC_SCAN                                1002
+#define IDC_CANCEL                              1003
+#define IDC_TARGET                              1004
+#define IDC_ALLDRIVES                           1005
+#define IDC_RECURSE                             1006
+#define IDC_NOARCHIVES                          1007
+#define IDC_ONLYINFECTED                        1008
+#define IDC_REMOVE                              1009
+#define IDC_NOMAIL                              1010
+#define IDC_NOPE                                1011
+#define IDC_NOOLE2                              1012
+#define IDC_NOHTML                              1013
+#define IDC_STATUS                              1014
+#define IDC_CMDLINE                             1015
